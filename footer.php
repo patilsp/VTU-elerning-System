7876545f80a993315741fdaf2d4c36b4cff25582
Santosh9696 <35653819+patilsp@users.noreply.github.com>
2018-10-18 12:36:28 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,55 @@
+<br>
+<div class="navbar  navbar-inverse">
+    <div class="navbar-inner">
+        <div class="footerindex">
+            <center><img width="25" height="25" src="admin/img/RCU-logo.jpg">&nbsp;VTU College E-Learning System Developed by MCA Department Design and Programmed by: Santosh Patil</center>
+        </div>
+        <!-- modal -->
+        <!-- mission -->
+        <div id="mission" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+            </div>
+            <div class="modal-body">
+                <div class="alert alert-info"><strong>Vision</strong></div>
+                <p><b>
+				To be the educational institution of preferred choice by ushering convergence of knowledge, skills and values. </b></p>
+				<p><b>
+				To Mould Character and Careers. 
+                </b></p>
+
+                <div class="alert alert-info"><strong>Mission</strong></div>
+                <p>
+					VTU College, which offers courses in MBA, MCA and Mtech all Branchs, has clarity on students future, both in their career and life. The institution has many case studies of inducting average students only to raise them to achieve distinction or ranks.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove-sign icon-large"></i>&nbsp;Close</button>
+            </div>
+        </div>
+        <!-- end mission -->
+        <!-- vision -->
+        <div id="vision" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+            </div>
+            <div class="modal-body">
+                <div class="alert alert-info"><strong>VTU E-Learning Mission</strong></div>
+
+                <p>To provide a highly developed form of teaching through maximizing the use of technology which will somehow give an easier and efficient way of learning that will make them to be competitive and productive citizens of the society.</p>
+
+                <div class="alert alert-info"><strong>E-Learning Vision</strong></div>
+               
+                To be able to prove to people that Filipinos also know how to cope up with the development in technology this will provide an easier way of teaching and a better form of learning.
+
+            </div>
+            <div class="modal-footer">
+                <button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove-sign icon-large"></i>&nbsp;Close</button>
+            </div>
+        </div>
+        <!-- end vision -->
+        <!--end modal -->
+
+    </div>
+</div>
+</div>
+
+
